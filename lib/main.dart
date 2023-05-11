7a5646698d975e9480d5b94bcc9c57f74294052d
Afzal <afzalashraf814@gmail.com>
2023-05-12 04:33:29 +0500

    ✨ feat: add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_instagram/responsive/web_screen_layout.dart';
 import 'package:flutter_instagram/utils/app_colors.dart';
 import 'package:flutter_instagram/responsive/responsive_layout_screen.dart';
 import 'package:flutter_instagram/responsive/mobile_screen_layout.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  kIsWeb
+      ? await Firebase.initializeApp(
+          options: const FirebaseOptions(
+          projectId: "instagram-clone-13f77",
+          appId: "1:180168720453:web:36226e70a39674f4b5e32c",
+          apiKey: "AIzaSyD99IPVfJAkj03L8O_afH7mg_aOgd5MRgo",
+          messagingSenderId: "180168720453",
+          storageBucket: "instagram-clone-13f77.appspot.com",
+        ))
+      : await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
